0ccecf650b9f6fb9f3d6254770ec0c3fb5d04503
iauther <iauther@163.com>
2019-10-20 23:54:43 +0800

    增加eeprom写入时的数据校对操作

--- User/src/e2p.c
+++ User/src/e2p.c
@@ -61,8 +61,17 @@ int e2p_read(u32 addr, u8 *data, u16 len)
 static int write_page(u32 addr, u8 *data, u16 len)
 {
     int r;
+    u8  tmp[PAGE_SIZE];
+    
     r = HAL_I2C_Mem_Write(i2c_handle, E2P_ADDR, addr, I2C_MEMADD_SIZE_16BIT, data, len, 0xFFFF);
+    r = HAL_I2C_Mem_Read(i2c_handle, E2P_ADDR, addr, I2C_MEMADD_SIZE_16BIT, tmp, len, 0xFFFF);
+    if(memcmp(data, tmp, len)) {
+        return -1;
+    }
+    
     delay_ms(5);
+    
+    return 0;
 }
 
 int e2p_write(u32 addr, u8 *data, u16 len)
