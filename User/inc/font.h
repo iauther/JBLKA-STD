7140fcb05baadaac82654c64bb9a17c08a8c0492
iauther <iauther@163.com>
2019-11-03 15:02:26 +0800

    增加大字体

--- User/inc/font.h
+++ User/inc/font.h
@@ -7,23 +7,22 @@
 //偏移量32 
 //ASCII字符集: !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~
 //PC2LCD2002取模方式设置：阴码+逐列式+顺向+C51格式
-//总共：2个字符集（16*16和24*24），用户可以自行新增其他分辨率的字符集。
 //每个字符所占用的字节数为:(size/8+((size%8)?1:0))*(size/2),其中size:是字库生成时的点阵大小(12/16/24...)
-//1608 ASCII字符集点阵
-extern const unsigned char font_1608[95][16]; 
-//2412 ASICII字符集点阵
-extern const unsigned char font_2412[95][36];
-//3216 ASICII字符集点阵
-extern const unsigned char font_3216[95][64];
-//4824 数码管字体ASICII字符集点阵
-extern const unsigned char font_4824[95][144];
 
+extern const unsigned char font_1608[95][16]; 
+extern const unsigned char font_2412[95][36];
+extern const unsigned char font_3216[95][64];
+extern const unsigned char font_4824[95][144];
+extern const unsigned char font_6432[95][256];
+extern const unsigned char font_8040[95][400];
 
 enum {
     FONT_16=0,
     FONT_24,
     FONT_32,
     FONT_48,
+    FONT_64,
+    FONT_80,
 
     FONT_MAX
 };
